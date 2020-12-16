ea7958cf97ea94e1996ffb9f5fe0984d27e117fb
Christian Ares <christianaresvillan@gmail.com>
2020-12-16 09:32:55 +0100

    Changes

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
 
 app.use(cors({
   credentials: true,
-  origin: ["http://localhost:3001"]
+  origin: ["http://localhost:3001", "https://escuela-infantil.netlify.app"]
 }));
 
 app.use((req, res, next)=>{
